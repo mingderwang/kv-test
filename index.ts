d4f90942bb9ae4f650cfd582ab48f85256578ab2
mingderwang <mingderwang@me.com>
2024-10-31 22:40:49 +0800

    init rc

--- index.ts
+++ index.ts
@@ -1,39 +1,56 @@
-import { sql, GeneratedAlways, Kysely } from "kysely"
-import { NeonDialect } from "kysely-neon"
+import { sql, GeneratedAlways, Kysely } from "kysely";
+import { NeonDialect } from "kysely-neon";
 
 interface Database {
-  person: PersonTable
+  person: PersonTable;
 }
 
 interface PersonTable {
-  id: GeneratedAlways<number>
-  first_name: string
-  gender: "male" | "female" | "other"
+  id: GeneratedAlways<number>;
+  first_name: string;
+  gender: "male" | "female" | "other";
 }
 
 const db = new Kysely<Database>({
   dialect: new NeonDialect({
     connectionString: process.env.DATABASE_URL,
   }),
-})
+});
 
-/*
-  await db.schema.createTable('person')
-  .addColumn('id', 'serial', (cb) => cb.primaryKey())
-  .addColumn('first_name', 'varchar(255)')
-  .addColumn('last_name', 'varchar(255)')
-  .addColumn('metadata', 'varchar(255)')
-  .addColumn('gender', 'varchar(50)')
-  .addColumn('created_at', 'timestamp', (cb) =>
-    cb.notNull().defaultTo(sql`CURRENT_TIMESTAMP`)
-  )
-  .execute()
-*/
+if (!process.env.DATABASE_URL) {
+  console.error("Please set DATABASE_URL in .env");
+}
 
-const a = await db
-  .insertInto("person")
-  .values({ first_name: "Jennifer", gender: "female" })
-  .returning("id", "first_name")
-  .executeTakeFirstOrThrow()
+const main = async () => {
+  //await db.schema.dropTable('person').execute()
 
-console.log(a)
+  await db.schema
+    .createTable("person")
+    .addColumn("id", "serial", (cb) => cb.primaryKey())
+    .addColumn("first_name", "varchar(255)")
+    .addColumn("last_name", "varchar(255)")
+    .addColumn("metadata", "varchar(255)")
+    .addColumn("gender", "varchar(50)")
+    .addColumn("created_at", "timestamp", (cb) =>
+      cb.notNull().defaultTo(sql`CURRENT_TIMESTAMP`)
+    )
+    .execute();
+
+  const a = await db
+    .insertInto("person")
+    .values({ first_name: "Jennifer", gender: "female" })
+    .returning(["id", "first_name", "gender"])
+    .executeTakeFirstOrThrow();
+
+  console.log(a);
+  try {
+    await sql`truncate table ${sql.table("person")}`.execute(db);
+    await db.schema.dropTable("person").execute();
+  } catch (err) {
+    console.log(`dropTable fail.`);
+  } finally {
+    process.exit(0);
+  }
+};
+
+main();
